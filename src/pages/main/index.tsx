0947257900098c5b282ecdf34ed22d57e804528a
Romanenkov Dmitriy <215rd@mail.ru>
2022-08-20 22:47:26 +0300

    feat: change some files

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Page } from 'pages/page';
 import { Grid, Typography } from '@mui/material';
+import { Advantages } from 'widgets/advantages';
+import { AboutTeam } from 'widgets/about-team';
 
 export const MainPage = () => {
   return (
     <Page pageClassName="main" title="Главная">
       <Grid container component="section">
-        <Typography variant="h5" component="h3">
-          Возможности и преимуществ приложения
+        <Typography sx={{width: 1, m: '5vh 0', typography: { xs: 'h5', md: 'h3'}}}>
+          RSLang - удобное приложение для изучения английского языка!
         </Typography>
+        <Advantages/>
       </Grid>
       <Grid container component="section">
-        <Typography variant="h5" component="h3">
-          О команде
-        </Typography>
+        <AboutTeam/>
       </Grid>
     </Page>
   )
