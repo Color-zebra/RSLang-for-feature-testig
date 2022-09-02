c17d981d38d7ff0dab96ccc028bece80b18a49bc
Romanenkov Dmitriy <215rd@mail.ru>
2022-09-02 22:28:11 +0300

    feat: final sprint-game

--- src/entities/game/ui/sprint/sprint-game.tsx
+++ src/entities/game/ui/sprint/sprint-game.tsx
@@ -1,117 +1,186 @@
-import { Button } from '@mui/material';
+import { Box, Button, Paper, Typography } from '@mui/material';
 import { AppDispatch } from 'app/store';
-import { finishGame, GameResultsData, GAME_PHASE, setGamePhase, useGame } from 'entities/game/model';
-import { Word } from 'entities/word'
+import { addGameResult, finishGame, GAME_PHASE, setGamePhase, setGameProgress, SOUND_EFFECT, useGame, useSoundEffect, useTimer } from 'entities/game/model';
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
-import { getRandomInt } from 'shared/lib';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { useLongestChain } from '../../model/hooks/longest-chain';
+import { makeAbsUrl } from 'shared/constants';
+import style from './sprint-game.module.scss'
+import classNames from 'classnames';
 
-export const GameSprintTest = () => {
+const GAME_TIME = 30;
+
+export const SprintGame = () => {
   
   const dispatch: AppDispatch = useDispatch();
-  const { words, progress, isSound } = useGame();
+  const { words } = useGame();
 
   const [word, setWord] = useState('');
   const [wordId, setWordId] = useState('');
   const [imgLink, setImgLink] = useState('');
-  const [results, setResults] = useState<GameResultsData>({});
   const [translate, setTranslate] = useState('');
   const [rightAnswer, setRightAnswer] = useState<null | boolean>(null)
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
-  const [winsCounter, setWinsCounter] = useState(0);  //TODO обсудить и решить какую именно статистику по словам будем собирать, пока затычка в виде счетчика отгаданных слов
+  const [scoreCounter, setScoreCounter] = useState(0);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
+  const [isRight, setIsRight] = useState<boolean | null>(null);
+  const [ playSoundEffect ] = useSoundEffect();
+  const [ setLongestChain ] = useLongestChain();
+  const [ chain, setChain] = useState(0);
+  const [ startTimer, stopTimer, counter ] = useTimer(GAME_TIME, -1);
   let userAnswer: boolean | null = null;
 
-  const [secunds, setSecunds] = useState(0);
+  const classes = classNames(style.base, {
+    [style.right]: isRight === true,
+    [style.wrong]: isRight === false,
+  })  
 
   const getWrongAnswer = () => {
-    const wrong = ['черный', 'слово', 'небо', 'лето', 'апельсин']; //TODO сделать функцию для генерации неправильного ответа, пока затычка
+    const wrong = words.filter((item) => item !== words[currentWordIndex]);
     const index = Math.floor(Math.random() * wrong.length);
     return wrong[index];
   }
 
   const createCard = () => {
+    if (!words[currentWordIndex]) {
+      setIsGameOver(true);
+      return
+    }
 
     const roll = Math.random();
-    const index = Math.floor(Math.random() * words.length);
-    setWord(words[index].word);
-    setWordId(words[index].id);
-    setImgLink(words[index].image);
+    setWord(words[currentWordIndex].word);
+    setWordId(words[currentWordIndex].id);
+    setImgLink(makeAbsUrl(words[currentWordIndex].image));
     
-    if (roll > 0.5) {
+    if (roll < 0.5 || words.length <= 1) {
       setRightAnswer(true);
-      setTranslate(words[index].wordTranslate);
+      setTranslate(words[currentWordIndex].wordTranslate);
     } else {
       setRightAnswer(false);
-      setTranslate(getWrongAnswer());
+      setTranslate(getWrongAnswer().wordTranslate);
     }
+  }
+
+  const handleAnswer = (answer: boolean) => {
+    userAnswer = answer;
+    checkAnswer();
+    if (currentWordIndex === words.length - 1) {
+      setIsGameOver(true);
+      return;
+    }
+    setCurrentWordIndex((s) => s + 1);
   }
 
   const checkAnswer = () => {
     if (userAnswer === rightAnswer) {
-      setWinsCounter((s) => s + 1);
-      setResults({...results, [wordId]: true})
-      console.log('Верно!');
+      setIsRight(true);
+      setScoreCounter((prev) => prev + (10 * (chain === 0 ? 1 : chain)));
+      dispatch(addGameResult({ id: wordId, result: true }))
+      setLongestChain((prev) => prev + 1);
+      setChain((prev) => prev + 1);
+      playSoundEffect(SOUND_EFFECT.RIGHT);
     } else {
-      setResults({...results, [wordId]: false})
-      console.log('Не верно!');
+      setIsRight(false);
+      dispatch(addGameResult({ id: wordId, result: false }))
+      setLongestChain(0)
+      setChain(0);
+      playSoundEffect(SOUND_EFFECT.WRONG);
     }
-    
-    if (!isGameOver) {
-      createCard();
-    }
+    const animation = setTimeout(() => setIsRight(null), 300);
   }
 
-  const handleEndGame = () => {
-    dispatch(setGamePhase(GAME_PHASE.LOADING));
-    dispatch(finishGame({
-      results,
-      longestChain: getRandomInt(0, Object.keys(results).length)
-    }));
-    
-    console.log('Результаты', results);
-  }
-
-  useEffect(() => {
-    createCard();
-  }, [words])
-
-  useEffect(() => {
-    if (secunds < 30) {
-      setTimeout(() => {
-        setSecunds(secunds => secunds + 1)
-      }, 1000);
-    } else {
-      setIsGameOver(true)
+  React.useEffect(() => {
+    if (isGameOver) {
+      stopTimer();
+      dispatch(setGamePhase(GAME_PHASE.LOADING));
+      dispatch(finishGame());
     }
-  }, [secunds, isGameOver])
+  }, [isGameOver, dispatch]);
 
+  React.useEffect(() => {
+      if (!isGameOver) {
+        return createCard;
+      }
+  }, [currentWordIndex]);
+
+  useEffect(() => {
+    setCurrentWordIndex(1);
+    startTimer(() => setIsGameOver(true));
+  }, [words, startTimer])
+
+  useEffect(() => {
+    dispatch(setGameProgress((counter / GAME_TIME) * 100));
+    return () => {
+      dispatch(setGameProgress(0));
+    }
+  }, [counter, dispatch]);
+
+  useEffect(() => {
+    const onKeyPress = (e: KeyboardEvent) => {
+      if (!e.repeat && e.code === 'ArrowRight') {
+        handleAnswer(false);
+      }
+      if (!e.repeat && e.code === 'ArrowLeft') {
+        handleAnswer(true)
+      }
+    };
+
+    document.addEventListener('keydown', onKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', onKeyPress);
+    }
+  }, [rightAnswer])
 
   return (
-    <div style={{margin: '20vh auto 20vh', textAlign: 'center'}}>
-      {isGameOver
-        ? 
-        <Button onClick={handleEndGame} variant="outlined" color="secondary" endIcon={<SkipNextIcon />}>
-          Завершить игру
-        </Button>
-        : <div style={{width: '280px', height: '400px', backgroundColor: 'tomato', margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'space-around'}}>
-            <div style={{display: 'flex', justifyContent: 'space-around'}}>
-              <span>Sprint</span><span>timer: {secunds}</span>
-            </div>
-            <div>
-              <div>
-                <img src={`http://localhost:3001/${imgLink}`} width='80%' alt="" />
-              </div>
-            </div>
-            <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around'}}>
-              <span style={{fontSize: '25px', color: 'blue'}}>{word}</span><span> is equal </span><span style={{fontSize: '25px', color: 'aqua'}}>{translate}</span>
-            </div>
-            <div style={{display: 'flex', justifyContent: 'space-around'}}>
-              <button onClick={() => {userAnswer = true; checkAnswer()}}>true</button>
-              <button onClick={() => {userAnswer = false; checkAnswer()}}>false</button>
-            </div>
-          </div>}
-    </div>
+    <Paper 
+      elevation={3} 
+      sx={{ 
+        flexBasis: {xs: 300, md: 400}, 
+        pl: {xs: 1, md: 3},
+        pr: {xs: 1, md: 3},
+        display: "flex", 
+        flexDirection: "column", 
+        rowGap: 2, p: 4,
+        height: "fit-content",
+      }}
+      className={classes}>
+        <Box sx={{ display: "flex", flexDirection: "column", alignItems: 'center', justifyContent: "center" }}>
+            <Box sx={{ display: "flex", flexDirection: {xs: "column", md: "row"}, columnGap: 6, justifyContent: "space-between" }}>
+              <Typography sx={{pt: 1}} variant='body1'>Chain length: 
+                <Typography component="span" sx={{pl: 1}} variant='h5' color="primary">{chain}</Typography>
+              </Typography>
+              <Typography sx={{pt: 1}} variant='body1'>Time left:
+                <Typography component="span" sx={{pl: 1}} variant='h5' color="error">{counter}</Typography>
+              </Typography>
+            </Box>
+            <Box
+              component="img"
+              sx={{
+                mt: 1,
+                mb: 1,
+                width: 350,
+                maxWidth: { xs: 250, md: 350 },
+                objectFit: "cover",
+                height: 150,
+              }}
+              alt="Word illustration"
+              src={imgLink || ''}
+            />
+            <Typography variant='h4'color={'green'}>{scoreCounter}</Typography>
+            <Box sx={{ display: "flex", flexDirection: "column", columnGap: 2, justifyContent: "center", alignItems: 'center' }}>
+              <Typography align='center' color='secondary' variant='h4'>{word}</Typography>
+              <Typography sx={{pt: 1}} variant='body2'>is mean</Typography>
+              <Typography align='center' color='secondary' variant='h4'>{translate}</Typography>
+            </Box>
+            <Box sx={{ display: "flex", flexDirection: {xs: "column", md: "row"}, columnGap: 6, justifyContent: "center" }}>
+              <Button variant="contained" color="success" sx={{ mt: 1, pl: 5, pr: 5, textTransform: "none" }} startIcon={<ArrowBackIcon />} onClick={() => {handleAnswer(true)}}>True!</Button>
+              <Button variant="contained" color="error" sx={{ mt: 1, pl: 5, pr: 5, textTransform: "none" }} endIcon={<ArrowForwardIcon />} onClick={() => {handleAnswer(false)}}>False!</Button>
+            </Box>
+          </Box>
+    </Paper>
   )
 }
 
--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -15,10 +15,9 @@ import {
 } from 'entities/game';
 import { AppDispatch } from 'app/store';
 import { useDispatch } from 'react-redux';
-import { CircularProgress } from '@mui/material';
+import { Box, CircularProgress } from '@mui/material';
 import { AudiocallGame } from 'entities/game/ui/audiocall';
-import { SprintGame } from 'entities/game/ui/sprint';
-import { GameSprintTest } from 'entities/game/ui/sprint/sprint-game';
+import { SprintGame } from 'entities/game/ui/sprint/sprint-game';
 
 export const GamePage = () => {
   const dispatch: AppDispatch = useDispatch();
@@ -40,14 +39,20 @@ export const GamePage = () => {
   return (
     <Page pageName={PAGES.GAME} pt={0}>
       <GameInterface>
+        {/* В зависимости от фазы игры, рэндерится тот или иной компонент
+            фазу игры меняем с помощью dispatch(setGamePhase(GAME_PHASE))
+        */}
         {gamePhase === GAME_PHASE.START && <GameStartScreen />}
         {gamePhase === GAME_PHASE.COUNTDOWN && <GameCountdown />}
         {gamePhase === GAME_PHASE.PLAYING && (
           (gameId === GAME.AUDIO && <AudiocallGame />) ||
-          (gameId === GAME.SPRINT && <GameSprintTest/>)
+          (gameId === GAME.SPRINT && <SprintGame/>)
         )}
         {gamePhase === GAME_PHASE.RESULTS && <GameResults />}
-        {gamePhase === GAME_PHASE.LOADING && <CircularProgress color="secondary" size={100} thickness={2}/>}
+        {gamePhase === GAME_PHASE.LOADING &&
+          <Box sx={{ height: "calc(100% - var(--header-height))", display: "flex", alignItems: "center"}}>
+            <CircularProgress color="secondary" size={100} thickness={2}/>
+          </Box>}
       </GameInterface>
     </Page>
   )
